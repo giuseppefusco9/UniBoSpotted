621c631f0fbf324cee155e52bf1747feece5b1b7
Giuseppe Fusco <giuseppe.fusco9@studio.unibo.it>
2026-01-15 18:35:46 +0100

    add first lines of code: start header

---
+++ template/base.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>UniBoSpotted</title> <!--RIVEDI CON TEMPLATEPARAMS-->
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+    <header>
+        <h1>UniBoSpotted</h1>
+    </header>
+</body>
