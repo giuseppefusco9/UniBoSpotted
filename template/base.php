af248d7b43bd0759e68cbd035519327186a17b3e
Giuseppe Fusco <giuseppe.fusco9@studio.unibo.it>
2026-01-16 11:15:45 +0100

    add section to base.php-modified .gitignore

--- template/base.php
+++ template/base.php
@@ -9,4 +9,31 @@
     <header>
         <h1>UniBoSpotted</h1>
     </header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="contatti.php">Search</a></li>
+            <li><a href="login.php">Profile</a></li>
+        </ul>
+    </nav>
+    <main>
+        <!-- rivedi con templateparams -->
+    </main><aside>
+        <section>
+            <h2>Recent Posts</h2>
+            <ul>
+                <li>
+                    <img src="./upload/sample1.jpg" alt="">
+                    <a href="articolo.php?id=1">Sample Post 1</a>
+                </li>
+                <li>
+                    <img src="./upload/sample2.jpg" alt="">
+                    <a href="articolo.php?id=2">Sample Post 2</a>
+                </li>
+            </ul>
+        </section>
+    </aside>
+    <footer>
+        <p>UniBoSpotted - A.A. 2024/2025</p>
+    </footer>
 </body>
