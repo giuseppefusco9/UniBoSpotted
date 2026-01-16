5d164bfaa4e65de737d3930158a47fd557cd5a7f
Giuseppe Fusco <giuseppe.fusco9@studio.unibo.it>
2026-01-16 12:41:53 +0100

    modified base: add comments to post

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
+//require_once 'bootstrap.php';
+
+//Base Template
+//$templateParams["titolo"] = "UBSpotted - Home";
+//$templateParams["nome"] = "lista-post.php";
+//$templateParams["categorie"] = $dbh->getCategories();
+//$templateParams["articoli"] = $dbh->getPosts(2);
+
 require 'template/base.php';
 ?>
