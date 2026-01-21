79dc0325727b50e95de35ac536738c562f3b8896
Giuseppe Fusco <giuseppe.fusco9@studio.unibo.it>
2026-01-21 15:58:39 +0100

    add entry profile in login-home

--- template/login-home.php
+++ template/login-home.php
@@ -1,4 +1,13 @@
-<h2 class="card-title h2 fw-bold mb-3">Ciao, <?php echo $templateParams["username"];?>!</h2>
+<div class="card shadow-sm border-0 rounded-3 mb-4">
+    <div class="card-body p-4">
+        <div class="text-center mb-4">
+            <div class="mb-3">
+                <span class="bi bi-person-circle display-4 text-unibo"></span>
+            </div>
+            <h2 class="h4 fw-bold">Ciao <?php echo $templateParams["username"]; ?>!</h2>
+        </div>
+    </div>
+</div>
 
 <?php 
     if(isset($templateParams["userposts"])){
